4bed98e9a03c4cb8a36b706586f25a8468fe2bff
Zak Saunders <saundersmayhemcoding@gmail.com>
2014-05-22 20:18:13 +0100

    Fix

--- src/main/java/com/saundersmayhem/demeraldcraft/lib/References.java
+++ src/main/java/com/saundersmayhem/demeraldcraft/lib/References.java
@@ -3,7 +3,7 @@ package com.saundersmayhem.demeraldcraft.lib;
 public class References {
 	
 	public static final String MODID = "demeraldcraft";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String Client = "com.saundersmayhem.demeraldcraft.lib.ProxyClient";
 	public static final String Common = "com.saundersmayhem.demeraldcraft.lib.ProxyCommon";
 
