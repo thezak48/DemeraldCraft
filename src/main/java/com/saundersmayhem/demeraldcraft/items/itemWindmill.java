4231c4ee4e4df01d37a8877ba378f3a38a44e700
Zak Saunders <saundersmayhemcoding@gmail.com>
2014-05-25 11:51:34 +0100

    Tided up code

--- src/main/java/com/saundersmayhem/demeraldcraft/items/itemWindmill.java
+++ src/main/java/com/saundersmayhem/demeraldcraft/items/itemWindmill.java
@@ -9,20 +9,24 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class itemWindmill extends Item {
-
-	
-	public static Item item;
 	
 	public itemWindmill(int i) {
 		this.setUnlocalizedName("Windmill");
-		this.setTextureName("demeraldcraft:Windmill");
+		this.setTextureName("demeraldcraft:itemWindmill");
 		this.setCreativeTab(Demeraldcraft.DemeraldCraft);
 		this.setCreativeTab(CreativeTabs.tabAllSearch);
 	}
 
-	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer player){
-		System.out.println("Yolo");
-		
-		return itemstack;
+	public boolean onItemUse(ItemStack itemstack,EntityPlayer player, World world, int x, int y, int z, int side, float x2, float y2, float z2){
+		if(!world.isRemote){
+			if(side==1 && world.getBlock(x, y, z)==(Demeraldcraft.BlockWindmillBase) && world.getBlockMetadata(x, y, z) == 5){	
+				for(int y1 = 0; y1 < 7; y1++){
+						world.setBlock(x, y+y1+1, z, Demeraldcraft.BlockWindmill, y1+1, 2);
+					}	
+				return true;
+			}
+		}
+		return false;
 	}
+	
 }
